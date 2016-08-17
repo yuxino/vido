8c12cf7792c38c29aa3d28e346c42b73e8c1f8ec
Nbsaw <1157988103@qq.com>
2016-08-17 13:44:36 +0800

    add demo and change some thing

---
+++ demo/demo.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    vi = new vido({
+        el: "#V-Video", //select elm
+        src: "http://7xqvgr.com1.z0.glb.clouddn.com/demo.mp4", //video src
+        w: "640px", //video width
+        h: "360px", //video height
+    });
+}
